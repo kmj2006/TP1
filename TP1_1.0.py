b4cf53443b57874b95ba211237a18c586bb367dc
YoonIn71 <insun082799@gmail.com>
2022-10-29 09:57:26 +0900

    ㅈㅅ

--- TP1_1.0.py
+++ TP1_1.0.py
@@ -1,25 +1,14 @@
-a=[]
+g=[]
 def squared(n):
-
 def gugudan(i):
     for x in range(1,10):
-        a.append(f'{i}x{x}={i*x}')
-    return a
+        g.append(f'{i}x{x}={i*x}')
+    return g
 def abs_num(k):
-
-
+    
 def sigma(n, k):
-
+    
 def main():
-    mode= int(input()):
-    if mode == 1:
-        squared(n)
-    elif mode == 2:
-        i = int(input())
-        print(gugudan(i))
-    elif mode == 3:
-        abs_num(k)
-    elif mode == 4:
-        sigma(n,k)
+    
 if __name__=="__main__":
     main()
