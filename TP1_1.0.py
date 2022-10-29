e9c753d819e4f85a653469c218b276071acc9e1a
kmj06 <kjjuni2006@gmail.com>
2022-10-29 10:02:56 +0900

    Tp1_main_1.1

--- TP1_1.0.py
+++ TP1_1.0.py
@@ -4,13 +4,20 @@ def gugudan(i):
 
 def abs_num(k):
     print(abs(k))
-
+    return
 def sigma(n, k):
 
 def main():
-    k = int(input())
-    abs_num(k)
-
+    k,n,i = map(int,input().split())
+    mode = int(input()):
+    if mode == 1:
+        squared(n)
+    elif mode == 2:
+        gugudan(i)
+    elif mode ==3:
+        abs_num(k)
+    elif mode ==4:
+        sigma(n,k)
 
 if __name__=="__main__":
     main()
